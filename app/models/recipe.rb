ae1d5cd723999a87bfd70e01f1ccdbc5fa8c38e6
Lindsey Maddox <llmaddox@gmail.com>
2016-10-14 10:01:49 -0400

    added uniqueness validation for name. currently working on commented out method for capitalizing each word in the name

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,12 +1,22 @@
 class Recipe < ApplicationRecord
 
-	
-	validates :name, length: { minimum: 4 }
-	validates :protein_id, presence: true, allow_nil: true
+	#before_validation :capitalize
 
+	validates :name, length: { minimum: 4 }, uniqueness: true
+	
 	belongs_to :grain
 
 	belongs_to :protein
 
 	has_many :meal_plans
+
+	protected
+	#def capitalize
+		#temp_arr = name.to_a
+
+		#cap_arr = temp_arr.map do |word|
+		#	word.capitalize!
+		#end
+		#self.name = cap_arr.join
+	#end
 end
