23d09636f4e05eae18f7ed934ea2d866030a0735
Lindsey Maddox <llmaddox@gmail.com>
2016-10-14 09:31:24 -0400

    corrected bugs in the way methods were set up for invoking a method to check suggestion against both last week's recipes and the current list of suggestions

--- app/models/meal_plan.rb
+++ app/models/meal_plan.rb
@@ -36,18 +36,26 @@ def self.meal_plan_generator
 
 		rando_recipe_grain = rando_recipe.grain
 		
-		match = self.compare_to_last_week(@last_week_meals,rando_recipe)
+		#this is dumb. should set up differently
+		lw_string = "lw"
+		tw_string = "tw"
+
+		lw_match = self.compare_to_week(@last_week_meals,rando_recipe,lw_string)
 		
-		if(match == false && @this_week_meals.length > 0)
-			compare_to_this_week(@this_week_meals, rando_recipe)
-			if(match == false)
-				add_to_list(@this_week_meals,rando_recipe)
-			end
-		elsif(match == false ) #if there isn't anything in this week meals, go ahead and add it
+		if(@this_week_meals.empty?)
+			tw_match = false
+		else
+			tw_match = self.compare_to_week(@this_week_meals, rando_recipe,tw_string)
+		end
+
+		#grain_max = self.check_component_part(@this_week_meals,rando_recipe,grain,rando_recipe_grain)
+
+		if(lw_match == false && tw_match == false)
 			add_to_list(@this_week_meals,rando_recipe)
 		else
 			next
 		end
+
 			#version without sending to another method. yucky so many loops
 			#if(grain_counts.has_key?(rando_recipe_grain) && grain_counts.values_at(rando_recipe_grain)[0] < 3)
 			#		grain_counts[rando_recipe_grain] = grain_counts[rando_recipe_grain]  + 1
@@ -76,11 +84,19 @@ end
 #for now using two methods
 
 
-def self.compare_to_last_week(arr,item)
+def self.compare_to_week(arr,item,week)
 	match = true
 	item_id_val = item.id
 	arr.each do |a|
-		a_id_val = a.recipe_id
+
+		#the recipe id is in a different location if it's already in meal plans
+		#we may want to go back and map the ids or something (in the original or another earlier method) so that it is the same for both
+		if(week == "tw")
+			a_id_val = a.id
+		else
+			a_id_val = a.recipe_id
+		end
+
 		if(a_id_val == item_id_val)
 			break
 		elsif(a_id_val != item_id_val && arr.index(a) == arr.index(arr.last))
@@ -90,27 +106,6 @@ def self.compare_to_last_week(arr,item)
 		end
 	end
 	match
-end
-
-#not eliminating items that are the same
-def self.compare_to_this_week(arr,item)
-	match = true
-	item_id_val = item.id
-	arr.each do |a|
-		a_id_val = a.id
-		if(a_id_val == item_id_val)
-			break
-		elsif(a_id_val != item_id_val && arr.index(a) == arr.index(arr.last))
-			match = false
-		else
-			next
-		end
-	end
-	match
-end
-
-def self.just_to_check_randoms(list)
-	list
 end
 
 def self.add_to_list(arr, item)
@@ -119,12 +114,11 @@ end
 
 #this isn't working now, so will just stuff the main method
 def self.check_component_part(arr,recipe, component, type)
-	#@this_week_meals = self.meal_plan_generator creates an infinite loop
 	#infinite loop if you do if(component[type] == 0)
+	grain_max = false
 		if(component.has_key?(type) && component.values_at(type)[0]< 3)
 			component[type] = component[type]  + 1
-			#for now, but probably would go to check protein next
-			add_to_list(arr,recipe)
+
 
 		#it is in the list but already at 3 items
 		elsif(component.has_key?(type))
