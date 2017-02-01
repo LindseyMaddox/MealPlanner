8e10d3ac3da8abbc445f67b8af966bdd5496e7bf
Lindsey Maddox <llmaddox@gmail.com>
2017-02-01 10:45:20 -0500

    increment default date for recipes after first suggestion

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -10,6 +10,7 @@ class MealsController < ApplicationController
 	end
 
 	def planner
+		@todays_date = Date.today
 		@number_of_meals = {"one meal" => 1, "five meals" => 5, "seven meals" => 7}
 
 		@this_week_meals = Meal.meal_generator(params[:number_of_meals], current_user)
