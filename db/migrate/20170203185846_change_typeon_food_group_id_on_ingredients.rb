1b41ab89909f7b392bed399427f50a5a44513688
Lindsey Maddox <llmaddox@gmail.com>
2017-03-03 09:03:47 -0500

    pg change

--- db/migrate/20170203185846_change_typeon_food_group_id_on_ingredients.rb
+++ db/migrate/20170203185846_change_typeon_food_group_id_on_ingredients.rb
@@ -1,9 +1,10 @@
 class ChangeTypeonFoodGroupIdOnIngredients < ActiveRecord::Migration[5.0]
-   class << self
-    include AlterColumn
-  end
-
+  
   def change
-  	change_column :ingredients, :food_group_id, :integer, "USING CAST(food_group_id AS integer)", 1
+  	execute "ALTER TABLE ingredients ALTER COLUMN food_group_id DROP DEFAULT;
+	ALTER TABLE ingredients ALTER COLUMN food_group_id TYPE integer
+  USING CAST(multiplier as INTEGER);
+	ALTER TABLE ingredients ALTER COLUMN food_group_id SET DEFAULT 1;"
+
   end
 end
