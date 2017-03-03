b154d95e6ab34dcd2b1c5b24ce8c40479af6f8df
Lindsey Maddox <llmaddox@gmail.com>
2017-03-03 09:04:50 -0500

    fix typo

--- db/migrate/20170203185846_change_typeon_food_group_id_on_ingredients.rb
+++ db/migrate/20170203185846_change_typeon_food_group_id_on_ingredients.rb
@@ -3,7 +3,7 @@ class ChangeTypeonFoodGroupIdOnIngredients < ActiveRecord::Migration[5.0]
   def change
   	execute "ALTER TABLE ingredients ALTER COLUMN food_group_id DROP DEFAULT;
 	ALTER TABLE ingredients ALTER COLUMN food_group_id TYPE integer
-  USING CAST(multiplier as INTEGER);
+  USING CAST(food_group_id as INTEGER);
 	ALTER TABLE ingredients ALTER COLUMN food_group_id SET DEFAULT 1;"
 
   end
