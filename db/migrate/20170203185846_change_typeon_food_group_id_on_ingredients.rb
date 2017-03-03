14ce7b94f62fd174592e50f1f14847fac379746d
Lindsey Maddox <llmaddox@gmail.com>
2017-03-03 08:52:50 -0500

    change migration and add module to enable type migration to work with pg

--- db/migrate/20170203185846_change_typeon_food_group_id_on_ingredients.rb
+++ db/migrate/20170203185846_change_typeon_food_group_id_on_ingredients.rb
@@ -1,5 +1,9 @@
 class ChangeTypeonFoodGroupIdOnIngredients < ActiveRecord::Migration[5.0]
+   class << self
+    include AlterColumn
+  end
+
   def change
-  	change_column :ingredients, :food_group_id, :integer
+  	change_column :ingredients, :food_group_id, :integer, "USING CAST(food_group_id AS integer)", 1
   end
 end
